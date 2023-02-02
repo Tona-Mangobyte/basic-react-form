10c100a582e29d9be404122f0ee4ef8a7b1a72ab
Tona <tona@mango-byte.com>
2023-02-02 22:28:08 +0700

    add validated

--- src/pages/basic-form/simple1.tsx
+++ src/pages/basic-form/simple1.tsx
@@ -1,10 +1,25 @@
+import { useForm } from "react-hook-form";
 
 export default function Simple1() {
 
 
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data: any) => console.log(data);
+
+    console.log(watch("example")); // watch input value by passing the name of it
+
     return (
-        <div>
-            Hello Simple Form
-        </div>
-    )
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {/* register your input into the hook by invoking the "register" function */}
+            <input defaultValue="test" {...register("example")} />
+
+            {/* include validation with required or other standard HTML validation rules */}
+            <input {...register("exampleRequired", { required: true })} />
+            {/* errors will return when field validation fails  */}
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <input type="submit" />
+        </form>
+    );
 }
