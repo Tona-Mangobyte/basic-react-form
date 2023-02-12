e751ad8307c0b7a970405101a2df1bee9936cc51
Tona <tona@mango-byte.com>
2023-02-12 09:52:29 +0700

    fixed

--- src/pages/components/simple-input.tsx
+++ src/pages/components/simple-input.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {AppProps} from "next/app";
 
 
-export default class SimpleInput1 extends React.Component {
+export default class SimpleInput1 extends React.Component<any, any> {
     constructor(props: AppProps) {
         super(props);
         this.state = {
@@ -11,11 +11,14 @@ export default class SimpleInput1 extends React.Component {
         this.handleChange = this.handleChange.bind(this)
     }
     handleChange(event: any) {
-        this.setState((state, props) => (
+        /*this.setState((state, props) => (
             {
                 input: event.target.value
             }
-        ))
+        ))*/
+        this.setState({
+            input: event.target.value
+        });
     }
 
     render() {
